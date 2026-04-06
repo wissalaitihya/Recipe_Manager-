613cbb8a6090a64441effa30e28f8bb28fd64f0f
wissalaitihya <wissalaitihya@gmail.com>
2026-04-06 16:24:03 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -30,4 +30,34 @@ create table recette(
 );
 
 
+INSERT INTO users (username, email, password) VALUES
+('wissal', 'wissal@mail.com', '$2y$10$examplehashuser');
 
+INSERT INTO categories (name) VALUES
+('Entrées'),
+('Plats'),
+('Desserts'),
+('Boissons');
+
+INSERT INTO recette 
+(users_id, categories_id, title, temp_de_production, ingredient, instructions, portions)
+VALUES
+(1, 1, 'Salade Marocaine', '15 min',
+ 'Tomates, concombre, oignon, huile olive',
+ 'Couper les légumes et mélanger avec huile et sel',
+ '2 personnes'),
+
+(1, 2, 'Poulet rôti', '1 heure',
+ 'Poulet, épices, ail, huile',
+ 'Assaisonner le poulet et cuire au four',
+ '4 personnes'),
+
+(1, 3, 'Gâteau au chocolat', '45 min',
+ 'Farine, chocolat, sucre, œufs',
+ 'Mélanger les ingrédients et cuire au four',
+ '6 personnes'),
+
+(1, 4, 'Jus d’orange', '10 min',
+ 'Oranges, sucre',
+ 'Presser les oranges et ajouter du sucre',
+ '3 verres');
